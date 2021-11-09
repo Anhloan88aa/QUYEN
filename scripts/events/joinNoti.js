b45e64bd8f91ad30125da653dd2fa029e30c455b
Anhloan88aa <91793980+Anhloan88aa@users.noreply.github.com>
2021-11-09 14:44:07 +0700

    Create joinNoti.js

---
+++ scripts/events/joinNoti.js
@@ -0,0 +1,58 @@
+module.exports.config = {
+	name: "join",
+	eventType: ["log:subscribe"],
+	version: "1.0.1",
+	credits: "Mirai Team",
+	description: "Thông báo bot hoặc người vào nhóm",
+	dependencies: {
+		"fs-extra": ""
+	}
+};
+
+module.exports.run = async function({ api, event,Threads }) {
+	const { join } = global.nodemodule["path"];
+	const { threadID } = event;
+	const data = (await Threads.getData(event.threadID)).data || {};
+    const checkban = data.banOut || []
+	if  (checkban.includes(checkban[0])) return
+	else if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
+		api.changeNickname(`[ ${global.config.PREFIX} ] • ${(!global.config.BOTNAME) ? "Bot của Quyền" : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
+		return api.sendMessage(`» 𝙆𝙀𝙏 𝙉𝙊𝙄 𝙏𝙃𝘼𝙉𝙃 𝘾𝙊𝙉𝙂«\n\n\n\n   ⚠Luật bot HXQ⚠ \n1. Cấm spam nhiều lệnh trong 1 phút\n2. Cấm 2 bot trong 1 box\n3. Hạn chế menu và help\n4. Đừng có mà chửi bot sẽ bị ban vĩnh viễn ☠
+\n  ◆━━━━━━━━◆\n📝Nhập ~info admin sẽ có thông tin của adminbot\n📌Liên hệ khi cần thiết\n⚜QTV và thành viên có thể dùng '~help rule' để xem hướng dẫn và set bảng luật box\n 
+ ◆━━━━━━━━◆\nThis bot made by HXQ. Thank you for using\nXin cảm ơn đã dành một chút thời gian để đọc...\n© Ad: Hoàng Xuân Quyền`, threadID);
+	}
+	else {
+		try {
+			const { createReadStream, existsSync, mkdirSync } = global.nodemodule["fs-extra"];
+			let { threadName, participantIDs } = await api.getThreadInfo(threadID);
+
+			const threadData = global.data.threadData.get(parseInt(threadID)) || {};
+			const path = join(__dirname, "cache", "joinGif");
+			const pathGif = join(path, `chao.gif`);
+
+			var mentions = [], nameArray = [], memLength = [], i = 0;
+			
+			for (id in event.logMessageData.addedParticipants) {
+				const userName = event.logMessageData.addedParticipants[id].fullName;
+				nameArray.push(userName);
+				mentions.push({ tag: userName, id });
+				memLength.push(participantIDs.length - i++);
+			}
+			memLength.sort((a, b) => a - b);
+			
+			(typeof threadData.customJoin == "undefined") ? msg = "✨Welcome {name}.\n🦋Chào mừng đã đến với {threadName}.\n🌸{type} là thành viên thứ {soThanhVien} của nhóm 🥳" : msg = threadData.customJoin;
+			msg = msg
+			.replace(/\{name}/g, nameArray.join(', '))
+			.replace(/\{type}/g, (memLength.length > 1) ?  'các bạn' : 'bạn')
+			.replace(/\{soThanhVien}/g, memLength.join(', '))
+			.replace(/\{threadName}/g, threadName);
+
+			if (existsSync(path)) mkdirSync(path, { recursive: true });
+
+			if (existsSync(pathGif)) formPush = { body: msg, attachment: createReadStream(pathGif), mentions }
+			else formPush = { body: msg, mentions }
+
+			return api.sendMessage(formPush, threadID);
+		} catch (e) { return console.log(e) };
+	}
+}
